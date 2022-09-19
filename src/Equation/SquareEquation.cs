c17aef83f90fb0f0d693147ede0ee9bcb246a739
Vladimir <7581290+dvv153m@users.noreply.github.com>
2022-09-19 12:17:35 +0300

    add tests + implemetation square equation (#1)
    Co-authored-by: Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>

--- src/Equation/SquareEquation.cs
+++ src/Equation/SquareEquation.cs
@@ -2,6 +2,41 @@
 {
     public class SquareEquation
     {
+        public double[] Solve(double a, double b, double c)
+        {
+            if (double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c)
+                || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+            {
+                throw new ArgumentException("Do not work with NaN and Infinity coefficients");
+            }
 
+            if (Math.Abs(a) < Double.Epsilon)
+            {
+                throw new ArgumentException("Coefficient a must not be equal zero");
+            }
+
+            var D = b * b - 4 * a * c;
+
+            //если дискрименант отрицательный
+            if (D < -Double.Epsilon)
+            {
+                return new double[] { };
+            }
+            //если дискрименант равен 0
+            if (Math.Abs(D) <= Double.Epsilon)
+            {
+                var x = -b / (2 * a);
+                return new double[] { x, x };
+            }
+            //если дискрименант положительный
+            if (D > Double.Epsilon)
+            {
+                var x1 = -b + Math.Sqrt(D) / (2 * a);
+                var x2 = -b - Math.Sqrt(D) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+
+            throw new ArgumentException();
+        }
     }
 }
--- tests/Equation.Tests/TestSquareEquation.cs
+++ tests/Equation.Tests/TestSquareEquation.cs
@@ -2,10 +2,114 @@ namespace Equation.Tests
 {
     public class TestSquareEquation
     {
-        [Fact]
-        public void Test1()
+        /// <summary>
+        /// Проверяется, что для уравнения x^2+1 = 0 корней нет
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        [Theory]
+        [InlineData(1, 0, 1)]
+        public void Solve_DiscremenantNegative_NoRoot(double a, double b, double c)//
         {
+            //Arrange            
+            int expected = 0;
+            SquareEquation _sut = new SquareEquation();
 
+            //Act
+            int actual = _sut.Solve(a, b, c).Length;
+
+            //Assert
+            Assert.True(expected == actual);
+        }
+
+        /// <summary>
+        /// Проверяется, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        [Theory]
+        [InlineData(1, 0, -1)]
+        public void Solve_DiscremenantMoreZero_TwoRoot(double a, double b, double c)
+        {
+            //Arrange            
+            var expected = new double[] { 1, -1 };
+            SquareEquation _sut = new SquareEquation();
+
+            //Act
+            double[] actual = _sut.Solve(a, b, c);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Проверяется, что для уравнения x^2+2x+1 = 0 = 0 есть один корень кратности 2 (x1= x2 = -1)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        [Theory]
+        [InlineData(1, 2, 1)]
+        [InlineData(0.99999999999999999, 2, 1)]
+        public void Solve_DiscremenantZero_SameTwoRoot(double a, double b, double c)
+        {
+            //Arrange            
+            var expected = new double[] { -1, -1 };
+            SquareEquation _sut = new SquareEquation();
+
+            //Act
+            double[] actual = _sut.Solve(a, b, c);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Проверяется, что коэффициент a не может быть равен 0
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        [Theory]
+        [InlineData(0, 3, 4)]
+        public void Solve_CoefficientAIsZero_ShouldThrowArgumentException(double a, double b, double c)
+        {
+            //Arrange
+            SquareEquation _sut = new SquareEquation();
+
+            //Act            
+            var exception = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    _sut.Solve(a, b, c);
+                });
+
+            //Assert
+            Assert.NotNull(exception);
+            Assert.Equal("Coefficient a must not be equal zero", exception.Message);
+        }
+
+        [Theory]
+        [InlineData(double.NaN, 3, 4)]
+        [InlineData(1, double.NegativeInfinity, 4)]
+        [InlineData(1, 2, double.PositiveInfinity)]
+        public void Solve_CoefficientsIsNanOrInfiniti_ShouldThrowArgumentException(double a, double b, double c)
+        {
+            //Arrange
+            SquareEquation _sut = new SquareEquation();
+
+            //Act            
+            var exception = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    _sut.Solve(a, b, c);
+                });
+
+            //Assert
+            Assert.NotNull(exception);
+            Assert.Equal("Do not work with NaN and Infinity coefficients", exception.Message);
         }
     }
 }
